2cd3fbda3c3edb92ecac0424d4e633f44299fca3
saunak1017 <saunak@shivanigems.com>
2026-01-07 12:03:45 -0500

    Delete rapnet-pretty/rapnet-pretty directory

--- rapnet-pretty/rapnet-pretty/src/lib/slug.ts
+++
@@ -1,9 +0,0 @@
-export function makeSlug(len = 8): string {
-  const alphabet = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789"; // no 0/O/I/1
-  let s = "";
-  const bytes = crypto.getRandomValues(new Uint8Array(len));
-  for (let i = 0; i < len; i++) {
-    s += alphabet[bytes[i] % alphabet.length];
-  }
-  return s;
-}
