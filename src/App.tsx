d363f5bd4d416dffbd072a065c387aff5b066aa0
saunak1017 <saunak@shivanigems.com>
2026-01-08 10:07:40 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,16 @@ import { UploadPage } from "./pages/UploadPage";
 import { ConfigurePage } from "./pages/ConfigurePage";
 import { EditPage } from "./pages/EditPage";
 import { PublicPage } from "./pages/PublicPage";
+import { PasswordGate } from "./components/PasswordGate";
 import "./styles.css";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<UploadPage />} />
-        <Route path="/configure" element={<ConfigurePage />} />
-        <Route path="/edit" element={<EditPage />} />
+        <Route path="/" element={<PasswordGate><UploadPage /></PasswordGate>} />
+        <Route path="/configure" element={<PasswordGate><ConfigurePage /></PasswordGate>} />
+        <Route path="/edit" element={<PasswordGate><EditPage /></PasswordGate>} />
         <Route path="/r/:slug" element={<PublicPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
